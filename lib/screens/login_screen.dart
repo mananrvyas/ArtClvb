418f1c0789a4fe32c5a5ad954a4b92a68b8773d9
mananrvyas <vyasmana@msu.edu>
2024-03-24 03:32:17 -0400

    created login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+            TextButton(
+                onPressed: (){
+                },
+                child: Text('Dont have an account? Register here')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
