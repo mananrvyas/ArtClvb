4a2b0c01f636a95fad0a6aa1f0280f7e891e41e1
mananrvyas <vyasmana@msu.edu>
2024-03-24 17:23:51 -0400

    initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'screens/login_screen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  // firebase initialization :P
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
